7e11b68d897c8c94eda49c0d33d8a5f8a5a50f89
yuiseki <yuiseki@gmail.com>
2022-03-26 11:10:39 +0900

    PracticeBubbleChartでデータを切り替えられるように変更

--- src/components/PracticeBubbleChart.tsx
+++ src/components/PracticeBubbleChart.tsx
@@ -1,13 +1,23 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { AbstractBubbleChart } from "./AbstractBubbleChart";
 
-import dataList from "../data/practice.json";
-import dimList from "../data/practiceDim.json";
+import sampleDataList from "../data/practice/data.json";
+import sampleDimList from "../data/practice/dataDim.json";
+
+// yuiseki
+import yuisekiDataList from "../data/yuiseki/data.json";
+import yuisekiDimList from "../data/yuiseki/dataDim.json";
+
+const datasetOptions = ["sample", "yuiseki"];
 
 export const PracticeBubbleChart: React.VFC = () => {
   const div = useRef<HTMLDivElement>(null);
   const [width, setWidth] = useState<number>(window.innerWidth - 100);
   const [height, setHeight] = useState<number>(window.innerHeight - 100);
+
+  const [dataOption, setDataOption] = useState("sample");
+  const [dataList, setDataList] = useState(sampleDataList);
+  const [dimList, setDimList] = useState(sampleDimList);
 
   useEffect(() => {
     if (!div.current) {
@@ -17,27 +27,56 @@ export const PracticeBubbleChart: React.VFC = () => {
     setHeight(div.current.offsetHeight);
   }, [div.current]);
 
+  const onChange = useCallback((e: React.FormEvent<HTMLSelectElement>) => {
+    setDataOption(e.currentTarget.value);
+  }, []);
+
+  useEffect(() => {
+    switch (dataOption) {
+      case "sample":
+        setDataList(sampleDataList);
+        setDimList(sampleDimList);
+        break;
+      case "yuiseki":
+        setDataList(yuisekiDataList);
+        setDimList(yuisekiDimList);
+      default:
+        break;
+    }
+  }, [dataOption]);
+
   return (
-    <div
-      style={{
-        height: "95vh",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "stretch",
-        alignContent: "stretch",
-      }}
-    >
-      <AbstractBubbleChart
-        width={width}
-        height={height}
-        inputData={dataList}
-        inputDimList={dimList}
-        bubbleSizeKey={"sizeValue"}
-        bubbleColorKey={"colorValue"}
-        bubbleTitle={(d) => d.name + ""}
-        xAxisKey={"xValue"}
-        yAxisKey={"yValue"}
-      />
+    <div>
+      <select onChange={onChange}>
+        {datasetOptions.map((opt) => {
+          return (
+            <option value={opt} selected={dataOption === opt}>
+              {opt}
+            </option>
+          );
+        })}
+      </select>
+      <div
+        style={{
+          height: "93vh",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "stretch",
+          alignContent: "stretch",
+        }}
+      >
+        <AbstractBubbleChart
+          width={width}
+          height={height}
+          inputData={dataList}
+          inputDimList={dimList}
+          bubbleSizeKey={"sizeValue"}
+          bubbleColorKey={"colorValue"}
+          bubbleTitle={(d) => d.name + ""}
+          xAxisKey={"xValue"}
+          yAxisKey={"yValue"}
+        />
+      </div>
     </div>
   );
 };
